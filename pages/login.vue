87b9de9ae4201b60d19fe9bc791abff902918ad0
takashiono2 <takashiono710@gmail.com>
2021-09-13 09:39:57 +0900

    01開発の準備

---
+++ pages/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <a class="btn btn-light" @click="login">
+      ログイン
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {
+      async login(){
+        this.$router.push("/")
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
